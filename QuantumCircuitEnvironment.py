e0ba47d4a4fb771a112241d5100870520088dd56
unknown <CharlesRW@protonmail.com>
2021-11-24 13:41:51 +0100

    Content of work session 24 November 2021

--- QuantumCircuitEnvironment.py
+++ QuantumCircuitEnvironment.py
@@ -2,17 +2,32 @@
 """
 Created on Mon Nov 22 11:02:19 2021
 
-@author: CharlesRW
+@author: OwenHuisman, CharlesRW, RenzeSuters
 """
 from tensorforce import Environment
 import numpy as np
+from scipy import linalg
 from qiskit import QuantumCircuit, transpile
 from qiskit.providers.aer import QasmSimulator
 from qiskit.visualization import plot_histogram
-import itertools as it
+import qiskit.quantum_info as qi
+
+from qiskit.providers.aer.noise import NoiseModel
+from qiskit.providers.aer.noise.errors import pauli_error, depolarizing_error
+from qiskit import QuantumRegister, ClassicalRegister
+from qiskit import QuantumCircuit, Aer, transpile, assemble
+from qiskit.visualization import plot_histogram
+aer_sim = Aer.get_backend('aer_simulator')
+
 
 class QuantumCircuitEnvironment(Environment):
-    def __init__(self,num_qubits,num_gates, num_samples):
+    def __init__(self,num_qubits,num_gates, num_samples, gateNoiseParams,):
+        #num_qubits = int
+        #num_samples = int
+        #gateNoiseParams is a list of (mu,sigma) pairs for each gate
+        #  For now we will assume the 1-qubit gates, when written as R_n (theta), have n and theta Gaussian noise
+        #  For 2 single-qubit gates, would be ((mu_th,sig_th),(mu_n,sig_n))
+
         super().__init__():
             #Number of qubits to simulate
             self.num_qubits = num_qubits
@@ -24,7 +39,19 @@ class QuantumCircuitEnvironment(Environment):
             # i.e. in the case of Hadamard, would have H1, H2...Hn
             self.num_gates = num_gates 
             self.simulator = QasmSimulator()
-            self.IdealCircuit = QuantumCircuit(num_qubits, 0)
+            self.Circuit = QuantumCircuit(num_qubits, 0)
+            self.gateNoiseParams = gateNoiseParams
+            self.action_list = []
+            self.targetBigRho = self.runCircuit()
+            
+            all_idx = range(self.num_qubits)
+            self.cart_prod = [(x,y) for x in all_idx for y in all_idx]
+            for x in self.cartprod:
+                if x[0]==x[1]:
+                    self.cart_prod.remove[x]
+            # make sure the length is right - number of unique ordered pairs
+            n_pairs = self.cart_prod.length()
+            assert n_pairs == self.num_qubits*(self.num_qubits-1)
         
     def states(self):
         return dict(type='float', shape=(2**(self.num_qubits*3+1),))
@@ -42,71 +69,131 @@ class QuantumCircuitEnvironment(Environment):
     def close(self):
         super().close()
 
+
     # def reset(self):
     #     state = np.random.random(size=(8,))
     #     return state
 
+
     def execute(self, actions):
         #update ideal circuit based on action
-        self.updateIdealCircuit(actions)
-        #sample ensemble to get rhos after action
-        self.evaluateDMs()
-        #next-state = sampled rhos
-        next_state = 
-        #reward = reward(sampled rhos)
-        reward = 
+        assert len(actions) == 1
+        self.action_list.append(actions)
+        next_state, reward ,terminal = self.getSampledBigRho(self)
+
         terminal = False  # Always False if no "natural" terminal state
         
         return next_state, terminal, reward
+    
 
-    #For a given ideal circuit, evaluate the density matrix for each possible input state
-    #Do multiple times to sample different gate noise, and return the average rhos
-    def evaluateDMs(self.num_samples):
-
-    def updateIdealCircuit(action):
+    # From the given ideal circuit, return a qiskit object which represents one 'noisy' version of the new gate
+    def constructCircuitRealisation(self):
+        #collect all the gates the agent thinks it's applying
+        #ideal_instruction_set = self.IdealCircuit.InstructionSet()
+       
         #action is an int between 0 and self.num_gates
         #need to specify the gate to be applied and the qubit line on which to apply it
         #Assume gate order is Rz1,Rz2... S1,S2,.... CNOT(1,1) CNOT(1,2)...
-        #
-        n = self.num_qubits
-
-        if action < n
-            qbit_idx = action
-            self.IdealCircuit.Z(qbit_idx)
-        elif action < 2*n
-            qbit_idx = action - n
-            self.IdealCircuit.S(qbit_idx)
-        else
-            tmp_idx = action - 2*n
-            qbit_idx2 = tmp_idx % n
-            qbit_idx1 = (tmp_idx-qbit_idx2) / n
-            self.IdealCircuit.CNOT(qbit_idx1, qbit_idx2) 
-
-        qubitindices = np.arange(0,n)
-        gatecombos = it.product(qubitindices, qubitindices)
-        f
-
-        for combo in gatecombos:
-            if combo[0] != combo[1]:
-
-        if action < n
-            qbit_idx = action
-            self.IdealCircuit.Z(qbit_idx)
-        elif action < 2*n
-            qbit_idx = action - n
-            self.IdealCircuit.S(qbit_idx)
-        else
-            tmp_idx = action - 2*n
-            qbit_idx2 = tmp_idx % n
-            qbit_idx1 = (tmp_idx-qbit_idx2) / n
-            self.IdealCircuit.CNOT(qbit_idx1, qbit_idx2) 
         
-        else:
-            combo_idx = action-2*n
-            combo = gatecombos[combo_idx]
-            self.Idealcircuit.cnot(combo[0],combo[1])
+        noisyCircuit = QuantumCircuit(num_qubits, 0)
+        for action in self.action_list:
+             #TODO make these gate implementations noisy
+            # use np.random.choice(a, size=None, replace=True, p=None) here
 
-        
+            if action < n
+                qbit_idx = action
+                noisyCircuit = applyRz(noisyCircuit, qbit_idx)
+                #noisyCircuit.Rz(np.pi/8, qbit_idx)
+            elif action < 2*n
+                qbit_idx = action - n
+                noisyCircuit = applyS(noisyCircuit, qbit_idx)
+                #noisyCircuit.s(qbit_idx)
+            else
+                tmp_idx = action - 2*n
+                assert tmp_idx < n_pairs #check we're not out of bounds
+                assert self.cart_prod[tmp_idx] == (cart_prod[tmp_idx][0], cart_prod[tmp_idx][1]) # CHECK this automatically unpacks?
+                noisyCircuit.cnot(self.cart_prod[tmp_idx]) 
             
+        return noisyCircuit
+    
+    
+    def applyRz(circuit, qbit_idx):
+
+        #TODO
+        # use UGate(theta, phi, lam[, label])
+        # use theta = np.random.normal(loc=mu_th, scale = TODO)
+        #phi = np.random.normal(loc=mu_phi, scale = TODO)
+
+        return circuit
+
+    def applyS(circuit, qbit_idx):
+
+        #TODO
+        # use UGate(theta, phi, lam[, label])
+        # use theta = np.random.normal(loc=mu_th, scale = TODO)
+        #phi = np.random.normal(loc=mu_phi, scale = TODO)
         
+        return circuit
+
+
+    def getSampledBigRho(self):
+
+        #targetrho should be 2^n*n*n rho' = U rho U* for input rho
+        n = self.num_qubits
+        bits = 2**(n)
+        #circuit = self.IdealCircuit
+        rhosamples = np.zeros((bits, self.num_samples, n, n))
+        rhomixed = np.zeros((bits,n, n))
+        observation = np.zeros((2*bits*n**2))
+        #rewards = np.zeros((bits))
+        for basis_state in range(bits):
+            binary = np.binary_repr(basis_state, width=bits)
+            
+            for sample in range(self.num_samples):
+                rhosamples[basis_state, sample] = runCircuit(circuit, binary)                
+            
+            rhomixed[basis_state] = np.average(rhosamples[basis_state], axis=0)
+            rewards[basis_state] = self.calculateReward(rhomixed, basis_state)
         
+        #TODO terminal
+        observation[:bits*n**2-1] = np.flatten(np.real(rhomixed))
+        observation[bits*n**2:] = np.flatten(np.imag(rhomixed))
+        return observation, reward, terminal
+
+
+    def runCircuit(self, binary):
+
+        circuit = self.constructCircuitRealisation(self)
+        circuit.initialize(binary, circuit.qubits)
+
+        #job = backend.run(circuit)
+        #result = job.result()
+        #rho = result.get_unitary(circuit, decimals =3)
+
+        rho = qi.DensityMatrix.from_instruction(circuit)
+        
+        return rho
+
+
+    def calculateReward(self, rhomixed, basis_state):
+        '''calculate the RL reward given a mixed density matrix rhomixed and the pure target density matrix targetrho'''
+        
+        #error = np.linalg.norm((rhomixed[basis_state]-self.targetrho[basis_state]), ord='fro')
+        #reward =  (error<0.2*bits**2) #Maybe there is a nicer way to assort rewards?
+
+        fidelity = self.calculateFidelity(targetrho[basis_state], rhomixed[basis_state])
+        reward = 1-fidelity # fidelity always between 0 and 1, is 1 if perfect match
+
+        return reward
+    
+
+    def calculateFidelity(target_rho, realized_rho):
+        '''description'''
+
+        sqrt_target = linalg.sqrtm(target_rho)
+        matrix1 = np.matmul(realized_rho, sqrt_target)
+        matrix2 = np.matmul(sqrt_target, matrix1)
+        sqrt_matrix = linalg.sqrtm(matrix2)
+        fidelity = (np.trace(sqrt_matrix))**2
+
+        return fidelity
