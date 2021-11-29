1fd42c61c8a18e8cd771679d0762d7b9716652b2
unknown <CharlesRW@protonmail.com>
2021-11-29 14:04:05 +0100

    first version which appears to learn the NUL operation

--- QuantumCircuitEnvironment.py
+++ QuantumCircuitEnvironment.py
@@ -38,7 +38,7 @@ class QuantumCircuitEnvironment(Environment):
         # i.e. in the case of Hadamard, would have H1, H2...Hn
         self.num_gates = num_gates 
         self.simulator = QasmSimulator()
-        self.circuit = QuantumCircuit(num_qubits)
+        #self.circuit = QuantumCircuit(num_qubits)
         self.gateNoiseParams = gateNoiseParams
         self.action_list = []
         self.targetBigRho = targetBigRho
@@ -54,11 +54,11 @@ class QuantumCircuitEnvironment(Environment):
         
 
     def states(self):
-        return dict(type='float', shape=(2**(self.num_qubits*3),))
+        return dict(type='float', shape=(2**(self.num_qubits*3+1),))
 
 
     def actions(self):
-        return dict(type='int', num_values=self.num_gates)
+        return dict(type='int', num_values=self.num_gates+1)
 
 
     # Optional: should only be defined if environment has a natural fixed
@@ -73,8 +73,16 @@ class QuantumCircuitEnvironment(Environment):
         super().close()
 
 
-    def reset(self): #TODO
-        state = np.zeros(self.targetBigRho.shape).flatten()
+    def reset(self): #TODO I have no idea what reset does in tensorforce :P
+        #state = np.zeros(self.targetBigRho.shape).flatten()
+        state = np.zeros(2**(self.num_qubits*3+1))
+        state[0] = 1
+        
+        idealCircuit = self.constructIdealCircuit()
+        print(idealCircuit)
+        
+        self.action_list = []
+        
         return state
 
 
@@ -89,40 +97,54 @@ class QuantumCircuitEnvironment(Environment):
         return next_state, terminal, reward
     
 
-    # From the given ideal circuit, return a qiskit object which represents one 'noisy' version of the new gate
-    def constructCircuitRealisation(self):
-        #collect all the gates the agent thinks it's applying
-        #ideal_instruction_set = self.IdealCircuit.InstructionSet()
-       
-        #action is an int between 0 and self.num_gates
-        #need to specify the gate to be applied and the qubit line on which to apply it
-        #Assume gate order is Rz1,Rz2... S1,S2,.... CNOT(1,1) CNOT(1,2)...
+    def constructIdealCircuit(self):
+        '''TODO'''
         
-        n = self.num_qubits
-        noisyCircuit = QuantumCircuit(n)
+        idealCircuit = QuantumCircuit(self.num_qubits)
+        idealCircuit = self.constructCircuitRealisation(idealCircuit, noise=False)
         
+        return idealCircuit
+
+    def constructCircuitRealisation(self, noisyCircuit, noise=True):
+        '''add gates from the given action list to the noisyCircuit
+        action is an int between 0 and self.num_gates
+        need to specify the gate to be applied and the qubit line on which to apply it
+        Assume gate order is NUL, Rz1,Rz2... S1,S2,.... CNOT(1,1) CNOT(1,2)...'''
         
+        n = self.num_qubits        
         
         for action in self.action_list:
-            if action < n:
-                qbit_idx = action
-                self.applyR(noisyCircuit, qbit_idx)
-            elif action < 2*n:
-                qbit_idx = action - n
-                self.applyS(noisyCircuit, qbit_idx)
+            if action == 0:
+                self.applyNUL()
+            elif action < n+1:
+                qbit_idx = action -1
+                self.applyR(noisyCircuit, qbit_idx, noise)
+            elif action < 2*n+1:
+                qbit_idx = action - n -1
+                self.applyS(noisyCircuit, qbit_idx, noise)
             else:
-                tmp_idx = action - 2*n
+                tmp_idx = action - 2*n - 1
                 assert tmp_idx < n_pairs #check we're not out of bounds
                 assert self.cart_prod[tmp_idx] == (cart_prod[tmp_idx][0], cart_prod[tmp_idx][1]) # CHECK this automatically unpacks?
-                self.applyCNOT(noisyCircuit, tmp_idx)
+                self.applyCNOT(noisyCircuit, tmp_idx, noise)
 
         return noisyCircuit
     
     
-    def applyR(self, circuit, qbit_idx):
-        # U(theta,0,0) = R(theta)
+    def applyNUL(self):
+        pass
+    
+    
+    def applyR(self, circuit, qbit_idx, noise):
+        '''add a noisy rotation over Z by pi/8 gate to the end of the circuit on qubit index qbit_idx
+        the noise is given by a normal distribution with standard deviation given by gateNoiseParams
+        forms a discrete universal single qubit gate set with the S gate'''
 
-        sig = self.gateNoiseParams[0]
+        # U(theta,0,0) = R(theta)
+        if noise:
+            sig = self.gateNoiseParams[0]
+        else:
+            sig = [0,0,0]
         #sig = 0.01
 
         mu_theta = np.pi/4
@@ -139,10 +161,16 @@ class QuantumCircuitEnvironment(Environment):
         circuit.u(theta,phi,lam, qbit_idx)
 
 
-    def applyS(circuit, qbit_idx):
-        #U(0,pi/2,0) = S
+    def applyS(self, circuit, qbit_idx, noise):
+        '''add a noisy S gate to the end of the circuit on qubit index qbit_idx
+        the noise is given by a normal distribution with standard deviation given by gateNoiseParams
+        forms a discrete universal single qubit gate set with the R gate above'''
 
-        sig = self.gateNoiseParams[1]
+        #U(0,pi/2,0) = S
+        if noise:
+            sig = self.gateNoiseParams[1]
+        else:
+            sig = [0,0,0]
         #sig = 0.01
 
         mu_theta = 0
@@ -160,9 +188,15 @@ class QuantumCircuitEnvironment(Environment):
         circuit.u(theta,phi,lam, qbit_idx)
 
 
-    def applyCNOT(circuit, tmp_idx):
-        
-        p_error = self.gateNoiseParams[2]
+    def applyCNOT(circuit, tmp_idx, noise):
+        '''add a noisy CNOT gate to the end of the circuit on qubits tmp_idx 
+        noisy in the sense that we add it with probability 1-p_error and we don't add it with prob. p_error'''
+
+        if noise:
+            p_error = self.gateNoiseParams[2]
+        else:
+            p_error = 0
+            
         implement = np.random.choice(2, 1, p = [p_error, 1-p_error])[0]
         
         if implement:
@@ -170,39 +204,92 @@ class QuantumCircuitEnvironment(Environment):
 
 
     def getSampledBigRho(self):
-
+        '''The main observation function: when run, 
+        gets the density matrices for each of several
+        sample realisations.  It then compares these to
+        the ideal, and determines the reward.
+        
+        Reward = Fidelity of sample-averaged DMs compared to ideal
+        Observed = average of sampled DMs
+        Terminal = false (no natural end-state, ceiling on 
+                          timesteps is implemented elsewhere)'''
         #targetrho should be 2^n*n*n rho' = U rho U* for input rho
         n = self.num_qubits
-        bits = 2**n
-        #circuit = self.constructNoisy
-        rhosamples = np.zeros((bits, self.num_samples, n, n))
-        rhomixed = np.zeros((bits,n, n))
-        observation = np.zeros((2*bits*n**2))
-        rewards = np.zeros((bits))
-        for basis_state in range(bits):
-            binary = np.binary_repr(basis_state, width=bits)
+        num_basis_states = 2**n # number of classical basis states
+
+        # to store all sample DMs: is 2**n , s , n, n
+        rho_samples = np.zeros((num_basis_states, self.num_samples, num_basis_states, num_basis_states))
+        
+        # sample-averaged DM
+        rho_sample_averaged = np.zeros((num_basis_states,num_basis_states, num_basis_states))
+        
+        # store reward for each basis state
+        rewards = np.zeros((num_basis_states))
+
+        # Loop over all basis states
+        for basis_state_id in range(num_basis_states):
+            #convert binary_state_id (an int) into a string of binary
+            binary = np.binary_repr(basis_state_id, width=n)
             
-            for sample in range(self.num_samples):
-                rhosamples[basis_state, sample] = self.runCircuit(binary)                
+            #sampling loop
+            for sample_id in range(self.num_samples):
+                #store each sample rho
+                rho_samples[basis_state_id, sample_id,:,:] = self.runCircuit(binary)                
             
-            rhomixed[basis_state] = np.average(rhosamples[basis_state], axis=0)
-            rewards[basis_state] = self.calculateReward(rhomixed, basis_state)
+            # average the results of all samples
+            rho_sample_averaged[basis_state_id] = np.average(rho_samples[basis_state_id], axis=0)
+
+            #store the reward for this basis state
+            rewards[basis_state_id] = self.calculateReward(rho_sample_averaged, basis_state_id)
         
         terminal = False #CHECK
-        observation[:bits*n**2-1] = np.flatten(np.real(rhomixed))
-        observation[bits*n**2:] = np.flatten(np.imag(rhomixed))
+
+        observation = self.observeRho(rho_sample_averaged,n,num_basis_states)
+
         reward = np.average(rewards, axis=0)
+        #print("Reward, " + str(reward))
         return observation, reward, terminal
 
+    def observeRho(self, rho_sample_averaged,n,num_basis_states):
+        '''Takes as input the sample-averaged DMs, and flattens them.  Broken off mainly for modularity
+        n and num_basis_states are passed strictly for convenience'''
+
+        obs_size = num_basis_states**3 #total number of CPLX datapts to store
+        
+        # sample-averaged DMs, but flat + split into RE and IM parts
+        observation = np.zeros(2*obs_size)
+
+        #flatten sample-averaged DMs and store RE and IM parts separately
+        observation[:obs_size] = np.ndarray.flatten(np.real(rho_sample_averaged))
+        observation[obs_size:] = np.ndarray.flatten(np.imag(rho_sample_averaged))
+
+        return observation
 
     def runCircuit(self, binary):
+        '''create one instance of a noisy ciruit and then calculate its density matrix
+        First, initialize the circuit in one particular basis state given by binary.
+        Then, call consTtructCircuitRealisation to add (noisy) gates to the circuit.
+        Finally, get the density matrix of the (noisy) circuit (one particular instance, so always pure density matrix).'''
 
-        circuit = self.constructCircuitRealisation()
-        circuit.initialize(binary, circuit.qubits)
-
-
-        rho = qi.DensityMatrix.from_instruction(circuit)
+        noisyCircuit = QuantumCircuit(self.num_qubits)
+        #noisyCircuit.initialize(binary, noisyCircuit.qubits) #TODO fix bug of qbit init
         
+        #loop over each qubit register to initialise
+        for lv in range(self.num_qubits):
+            b = int(binary[lv])
+            assert b == 0 or b == 1
+            if b == 0:
+                vec = [1,0]
+            elif b==1:
+                vec = [0,1]
+
+            noisyCircuit.initialize(vec,lv)
+
+        noisyCircuit = self.constructCircuitRealisation(noisyCircuit)
+        rho = qi.DensityMatrix.from_instruction(noisyCircuit)
+        
+        rho = np.array(rho)
+ 
         return rho
 
 
@@ -215,13 +302,14 @@ class QuantumCircuitEnvironment(Environment):
         fidelity = self.calculateFidelity(self.targetBigRho[basis_state], rhomixed[basis_state])
         reward = fidelity # fidelity always between 0 and 1, is 1 if perfect match
 
-        return reward
+        return reward-0.5
     
 
-    def calculateFidelity(target_rho, realized_rho):
-        '''description'''
+    def calculateFidelity(self,target_rho, realized_rho):
+        '''calculate the fidelity of the realized_rho density matrix and the target_rho
+        Fidelity is always between 0 and 1. The fidelity is 1 iff the density matrices are exactly the same.'''
 
-        #TODO might be simplified because target rho is always pure
+        # might be simplified because target rho is always pure, but nice for generality
         sqrt_target = linalg.sqrtm(target_rho)
         matrix1 = np.matmul(realized_rho, sqrt_target)
         matrix2 = np.matmul(sqrt_target, matrix1)
--- QuantumLearningRunner.py
+++ QuantumLearningRunner.py
@@ -1,10 +1,11 @@
 from QuantumCircuitEnvironment import QuantumCircuitEnvironment
 import numpy as np
+import matplotlib.pyplot as plt
 
 num_qubits = 1
 num_gates = 2
-num_samples = 15
-sig = 0.3
+num_samples = 1
+sig = 1
 gateNoiseParams = [[sig, sig, sig], [sig, sig, sig], sig]
 targetBigRho = np.zeros([2**num_qubits, 2**num_qubits, 2**num_qubits])
 targetBigRho[0,0,0] = 1
@@ -14,21 +15,43 @@ myQuantumCircuitEnvironment = QuantumCircuitEnvironment(num_qubits, num_gates, n
 
 from tensorforce.environments import Environment
 environment = Environment.create(
-    environment=myQuantumCircuitEnvironment, max_episode_timesteps=2
+    environment=myQuantumCircuitEnvironment, max_episode_timesteps=40
 )
 
 from tensorforce.agents import Agent
 agent = Agent.create(
-    agent='ppo', environment=environment, batch_size=10, learning_rate=1e-3
+    agent='ppo', environment=environment, batch_size=5, learning_rate=1e-3
 )
 
+# Train for 100 episodes
+all_rewards = list()
+for _ in range(10):
+    episode_states = list()
+    episode_internals = list()
+    episode_actions = list()
+    episode_terminal = list()
+    episode_reward = list()
 
-from tensorforce.execution import Runner
-runner = Runner(
-    agent=agent,
-    environment=environment
-)
+    states = environment.reset()
+    internals = agent.initial_internals()
+    terminal = False
+    while not terminal:
+        episode_states.append(states)
+        episode_internals.append(internals)
+        actions, internals = agent.act(
+            states=states, internals=internals, independent=True
+        )
+        episode_actions.append(actions)
+        states, terminal, reward = environment.execute(actions=actions)
+        episode_terminal.append(terminal)
+        episode_reward.append(reward)
 
-runner.run(num_episodes=1, evaluation=True)
-
-runner.close()
+    agent.experience(
+        states=episode_states, internals=episode_internals,
+        actions=episode_actions, terminal=episode_terminal,
+        reward=episode_reward
+    )
+    agent.update()
+    all_rewards.append(episode_reward[-1])
+plt.plot(all_rewards)
+plt.show()
